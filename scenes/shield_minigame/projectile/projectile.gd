5c5aed1125c51482b96855229db82f4c96730a60
Geoff Gard <gardgeoff@gmail.com>
2025-01-25 16:37:18 -0700

    update projectile physics to use rigidbody2d instead of characterbody

--- scenes/shield_minigame/projectile/projectile.gd
+++ scenes/shield_minigame/projectile/projectile.gd
@@ -1,26 +1,11 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 var ran_dir
+
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	position.x = position.x + 20
-	position.y = position.y + 20
-	set_deferred('freeze', true)
-	const DIRECTIONS = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN]
-	ran_dir = DIRECTIONS.pick_random()
-	
+# set_deferred("freeze", true)
 
-func _process(delta: float) -> void:
-	rotate(delta * 5)
-	pass
-
-func unfeeze():
-	set_deferred('freeze', false)
 
 func _physics_process(delta: float) -> void:
-	var collision = move_and_collide(ran_dir * velocity * delta)
-	var collider
-	if collision:
-		collider = collision.get_collider()
-		collider.bounce(collision.get_normal())
-		print(collider)
+	pass
+# set_deferred("freeze", false)
--- scenes/shield_minigame/projectiles/projectiles.gd
+++ scenes/shield_minigame/projectiles/projectiles.gd
@@ -1,37 +1,43 @@
 extends Node2D
 
 @export var parent: Node
+@export var speed_modifier: int = 200
 @onready var pivot_point = $PivotPoint
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var timer = $Timer
 	timer.timeout.connect(_on_timer_timeout)
 
+
 func _on_timer_timeout():
 	print("Time to attack!")
 	choose_random_child()
+
 
 func choose_random_child():
 	var children = pivot_point.get_children()
 
 	if children.size() > 0:
 		var random_index = randi() % children.size()
-		var random_child: CharacterBody2D = children[random_index]
-		random_child.set_deferred('freeze', false)
-		random_child.velocity = position
-		wait(.1)
+		var random_child: RigidBody2D = children[random_index]
+		random_child.linear_velocity = Vector2(
+			random_child.position.x + speed_modifier, random_child.position.y + speed_modifier
+		)
+		print_debug(random_child)
 		random_child.reparent(parent)
 		print("Random Child selected: ", random_child.name)
 
 	else:
-		print('No children available')
+		print("No children available")
 		return null
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	rotate(delta * 3)
+
 
 func wait(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
