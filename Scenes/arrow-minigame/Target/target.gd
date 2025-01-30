2426d4c65a48fb45fbfa612a1e81a53421dd439c
Cole <hoopwalker@yahoo.com>
2025-01-29 22:02:25 -0700

    added final score popup, shows your total points clicked

--- Scenes/arrow-minigame/Target/target.gd
+++ Scenes/arrow-minigame/Target/target.gd
@@ -9,14 +9,8 @@ func _ready() -> void:
 	animation_player.play('scale_change')
 	await animation_player.animation_finished
 	queue_free()
-	print('ready')
 
 func add_target_ran_location():
 	var random_x = randf_range(area.position.x, area.size.x)
 	var random_y = randf_range(area.position.y, area.size.y)
 	position = Vector2(random_x, random_y)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-# func _process(delta: float) -> void:
-# 	pass
--- Scenes/arrow-minigame/minigame_scene/arrow_minigame.gd
+++ Scenes/arrow-minigame/minigame_scene/arrow_minigame.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var target: PackedScene
+@export var final_score: PackedScene
 @onready var timer = $Timer
 var timer_iterations = 0
 
@@ -11,5 +12,7 @@ func _on_timer_timeout():
   var target_instance = target.instantiate()
   get_parent().add_child(target_instance)
   timer_iterations += 1
-  if timer_iterations == 6:
+  if timer_iterations == 5:
     timer.queue_free()
+    var score_instance = final_score.instantiate()
+    get_parent().add_child(score_instance)
