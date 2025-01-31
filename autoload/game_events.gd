03a70e2791e662487ad1226fbc3637d2301ca9ab
Geoff Gard <gardgeoff@gmail.com>
2025-01-30 22:37:37 -0700

    remove unused signal

--- autoload/game_events.gd
+++ autoload/game_events.gd
@@ -10,14 +10,14 @@ signal projectile_destroyed
 func emit_scene_changed():
 	scene_changed.emit()
 
+
 func emit_score_increase(type: String):
 	score_increase.emit(type)
+
 
 func emit_target_hit(value: int):
 	target_hit.emit(value)
 
+
 func emit_shape_broken(shape: Node2D):
 	shape_broken.emit(shape)
-
-func emit_projectile_destroyed():
-	projectile_destroyed.emit()
