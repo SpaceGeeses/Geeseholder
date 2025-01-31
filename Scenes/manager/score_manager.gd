0d0010f5bbb8ba2bef18122ab33745f799bd8c28
Geoff Gard <gardgeoff@gmail.com>
2025-01-30 22:36:32 -0700

    set up score manager comonent

---
+++ Scenes/manager/score_manager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+@onready var player_score_label: Label = $PlayerScore
+@onready var enemy_score_label: Label = $EnemyScore
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var final_score: Label = $FinalScore
+
+var enemy_score = 0
+var player_score = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameEvents.score_increase.connect(uptick_score)
+
+
+func uptick_score(type: String):
+	if type == "enemy":
+		enemy_score += 1
+		enemy_score_label.text = str(enemy_score)
+	elif type == "player":
+		player_score += 1
+		player_score_label.text = str(player_score)
+	check_game_ended()
+
+
+func check_game_ended():
+	var projectile_total: Array = get_tree().get_nodes_in_group("Projectiles")
+	print(projectile_total.size())
+	if projectile_total.size() == 1:
+		final_score.text = "You score " + str(player_score) + " points"
+		animation_player.play("pop_up_final")
