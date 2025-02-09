f47345ac20f131441fc240e1d1e1418378f1b373
Cole <hoopwalker@yahoo.com>
2025-02-09 13:47:36 -0700

    added a score manager for sword mini-game, needs a game ending timer, now updates properly on screen

---
+++ Scenes/manager/player_hit_incorrect.gd
@@ -0,0 +1,15 @@
+extends Node2D
+ 
+var enemy_score = 0
+@onready var label = $bad_hit
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GameEvents.shape_broken.connect(uptick_score_on_break)
+
+
+func uptick_score_on_break(area: Area2D):
+	var type = area.get_groups()[0]
+	if type == 'Rock':
+		enemy_score += 1
+	label.text = str(enemy_score)
