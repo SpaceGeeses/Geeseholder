0752ad3cdbeccff1a59da218b80f463592a9a164
Geoff Gard <gardgeoff@gmail.com>
2025-02-10 22:43:42 -0700

    save player location after minigame scene

---
+++ autoload/overworld_state.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var player_coordinates: Vector2 = Vector2(0, 0)
+var enemy_coordinates: Array[Vector2]
+
+
+func set_player_coordinates(coordinates: Vector2):
+	player_coordinates = coordinates
